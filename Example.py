7543335ac73c8699ad77a478d4ffd334d85c6de4
mhnarfth <mhnarfth@gmail.com>
2020-04-28 07:11:08 +0600

    Second commit

--- Example.py
+++ Example.py
@@ -3,3 +3,9 @@ print ("Hello World")
 list = [num for num in range(5)]
 
 print (list)
+
+class human:
+
+    def __init__(self, sex):
+        self.sex = sex
+
---
+++ Human.py
@@ -0,0 +1,11 @@
+class Man:
+
+    def __init__(self, name) :
+        self.name = name
+
+
+class Woman :
+
+    def __init__(self, name):
+
+        self.name= name
